39f28abee19e5891e657937fd12b4bb84e588228
samrat-62 <katuwalsamrat22@gmail.comProfile imagekatuwalsamrat22@gmail.com>
2025-05-06 21:01:09 +0545

    Commenting unwanted code for today

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 // yah database ko code ra logic lekxum
 
-const {Sequelize, DataTypes} = require("sequelize")
+const {Sequelize} = require("sequelize")
 
 const sequelize = new Sequelize("postgresql://postgres.vhhhbhoypvrltqlblvfp:Iamlearningsupabase@aws-0-ap-south-1.pooler.supabase.com:6543/postgres") 
 
@@ -12,8 +12,8 @@ sequelize.authenticate()
     console.log("Error aayo" + err)
 })
 
-const db = {}
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+// const db = {}
+// db.Sequelize = Sequelize
+// db.sequelize = sequelize
 
-module.exports = db
+// module.exports = db
